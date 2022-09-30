76002bd865400dad6672747de974073771f33903
Mr.Panda <xivistudios@gmail.com>
2022-09-30 20:17:09 +0800

    Update media_stream_track.h

--- webrtc_sys/src/media_stream_track.h
+++ webrtc_sys/src/media_stream_track.h
@@ -3,6 +3,7 @@
 #include "sys.h"
 #include "media/base/video_broadcaster.h"
 #include "media/base/adapted_video_track_source.h"
+#include "api/video/i420_buffer.h"
 
 class VideoSource 
     : public rtc::AdaptedVideoTrackSource
@@ -16,25 +17,51 @@ public:
 
     bool remote() const;
     bool is_screencast() const;
-    SourceState state() const;
+    webrtc::MediaSourceInterface::SourceState state() const;
     absl::optional<bool> needs_denoising() const;
 };
 
-bool VideoSource::remote() const {
+bool VideoSource::remote() const 
+{
     return false;
+}
+
+bool VideoSource::is_screencast() const
+{
+    return false;
+}
+
+absl::optional<bool> VideoSource::needs_denoising() const
+{
+    return false;
+}
+
+webrtc::MediaSourceInterface::SourceState VideoSource::state() const
+{
+    return SourceState::kInitializing;
 }
 
 typedef struct
 {
-    rtc::AdaptedVideoTrackSource* raw;
+    rtc::scoped_refptr<rtc::AdaptedVideoTrackSource> source;
+    rtc::scoped_refptr<webrtc::I420Buffer> i420_buf;
+    webrtc::VideoFrame* frame;
 } IVideoSource;
 
 typedef struct
 {
-
+    uint32_t width;
+    uint32_t height;
+    uint8_t* buffer;
 } IVideoFrame;
 
-IVideoSource* media_create_video_source()
+typedef struct
+{
+    uint32_t width;
+    uint32_t height;
+} VideoDescripts;
+
+IVideoSource* media_create_video_source(VideoDescripts* vdesc)
 {
     IVideoSource* vs = (IVideoSource*)malloc(sizeof(IVideoSource));
     if (vs == NULL)
@@ -42,12 +69,14 @@ IVideoSource* media_create_video_source()
         return NULL;
     }
 
-    vs->raw = VideoSource::Create();
+    vs->source = VideoSource::Create();
+    vs->i420_buf = webrtc::I420Buffer::Create(vdesc->width, vdesc->height);
+    vs->frame = webrtc::VideoFrame(vs->i420_buf, 0, 0);
     return vs;
 }
 
-void media_video_source_add_frame(IVideoSource* video_source,
+void media_video_source_add_frame(IVideoSource* vs,
     IVideoFrame* frame)
 {
-
+    // vs->source.get()->OnFrame();
 }
