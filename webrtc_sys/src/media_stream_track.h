62443628252b88f5a83e527a01d2794b2044e8b7
Mr.Panda <xivistudios@gmail.com>
2022-10-14 18:36:53 +0800

    update;

--- webrtc_sys/src/media_stream_track.h
+++ webrtc_sys/src/media_stream_track.h
@@ -5,21 +5,41 @@
 #include "media/base/adapted_video_track_source.h"
 #include "api/video/i420_buffer.h"
 
+typedef struct
+{
+    rtc::scoped_refptr<VideoSource> source;
+} IVideoSource;
+
+typedef struct
+{
+    uint32_t width;
+    uint32_t height;
+    uint8_t* buffer;
+} IVideoFrame;
+
 class VideoSource 
     : public rtc::AdaptedVideoTrackSource
     , public rtc::RefCountInterface
 {
 public:
-    static VideoSource* Create()
-    {
-        new rtc::RefCountedObject<VideoSource>();
-    }
+    static VideoSource* Create();
+    void PushFrame(webrtc::VideoFrame& frame);
 
     bool remote() const;
     bool is_screencast() const;
     webrtc::MediaSourceInterface::SourceState state() const;
     absl::optional<bool> needs_denoising() const;
 };
+
+VideoSource* VideoSource::Create()
+{
+    new rtc::RefCountedObject<VideoSource>();
+}
+
+void VideoSource::PushFrame(webrtc::VideoFrame& frame)
+{
+    this->OnFrame(frame);
+}
 
 bool VideoSource::remote() const 
 {
@@ -41,27 +61,7 @@ webrtc::MediaSourceInterface::SourceState VideoSource::state() const
     return SourceState::kInitializing;
 }
 
-typedef struct
-{
-    rtc::scoped_refptr<rtc::AdaptedVideoTrackSource> source;
-    rtc::scoped_refptr<webrtc::I420Buffer> i420_buf;
-    webrtc::VideoFrame* frame;
-} IVideoSource;
-
-typedef struct
-{
-    uint32_t width;
-    uint32_t height;
-    uint8_t* buffer;
-} IVideoFrame;
-
-typedef struct
-{
-    uint32_t width;
-    uint32_t height;
-} VideoDescripts;
-
-IVideoSource* media_create_video_source(VideoDescripts* vdesc)
+IVideoSource* media_create_video_source()
 {
     IVideoSource* vs = (IVideoSource*)malloc(sizeof(IVideoSource));
     if (vs == NULL)
@@ -70,13 +70,14 @@ IVideoSource* media_create_video_source(VideoDescripts* vdesc)
     }
 
     vs->source = VideoSource::Create();
-    vs->i420_buf = webrtc::I420Buffer::Create(vdesc->width, vdesc->height);
-    vs->frame = webrtc::VideoFrame(vs->i420_buf, 0, 0);
     return vs;
 }
 
-void media_video_source_add_frame(IVideoSource* vs,
-    IVideoFrame* frame)
+void media_video_source_add_frame(IVideoSource* vs, IVideoFrame* frame)
 {
-    // vs->source.get()->OnFrame();
+    auto i420_buf = webrtc::I420Buffer::Copy(frame->width, frame->height, 
+        frame->buffer, frame->width);
+
+    vs->source.get()->PushFrame(webrtc::VideoFrame(i420_buf, 0, 0, 
+        webrtc::kVideoRotation_0));
 }
