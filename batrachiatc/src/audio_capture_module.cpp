e14fd3b56c6b27eb189b7b1b55d5e870824d92b7
Mr.Panda <xivistudios@gmail.com>
2022-12-07 19:01:35 +0800

    rw: split header or impl;

--- batrachiatc/src/audio_capture_module.cpp
+++ batrachiatc/src/audio_capture_module.cpp
@@ -1,1 +1,359 @@
+#include "audio_capture_module.h"
 
+AudioCaptureModule* AudioCaptureModule::Create()
+{
+    auto self = new rtc::RefCountedObject<AudioCaptureModule>();
+    if (!self->Initialize()) 
+    {
+        return NULL;
+    }
+
+    self->AddRef();
+    return self;
+}
+
+int32_t AudioCaptureModule::ActiveAudioLayer(AudioLayer* audioLayer) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::RegisterAudioCallback(webrtc::AudioTransport* audio_callback)
+{
+    _audio_callback = audio_callback;
+    return 0;
+}
+
+int32_t AudioCaptureModule::Init()
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::Terminate()
+{
+    // Clean up in the destructor. No action here, just success.
+    return 0;
+}
+
+bool AudioCaptureModule::Initialized() const
+{
+    return 0;
+}
+
+int16_t AudioCaptureModule::PlayoutDevices()
+{
+    return 0;
+}
+
+int16_t AudioCaptureModule::RecordingDevices()
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::PlayoutDeviceName(uint16_t index,
+    char name[webrtc::kAdmMaxDeviceNameSize],
+    char guid[webrtc::kAdmMaxGuidSize])
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::RecordingDeviceName(uint16_t index, 
+    char name[webrtc::kAdmMaxDeviceNameSize], 
+    char guid[webrtc::kAdmMaxGuidSize])
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetPlayoutDevice(uint16_t index)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetPlayoutDevice(WindowsDeviceType device)
+{
+    if (_play_is_initialized)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int32_t AudioCaptureModule::SetRecordingDevice(uint16_t index)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetRecordingDevice(WindowsDeviceType device)
+{
+    if (_rec_is_initialized)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int32_t AudioCaptureModule::PlayoutIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::InitPlayout()
+{
+    _play_is_initialized = true;
+    return 0;
+}
+
+bool AudioCaptureModule::PlayoutIsInitialized() const
+{
+    return _play_is_initialized;
+}
+
+int32_t AudioCaptureModule::RecordingIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::InitRecording()
+{
+    _rec_is_initialized = true;
+    return 0;
+}
+
+bool AudioCaptureModule::RecordingIsInitialized() const
+{
+    return _rec_is_initialized;
+}
+
+int32_t AudioCaptureModule::StartPlayout()
+{
+    if (!_play_is_initialized) 
+    {
+        return -1;
+    }
+
+    _playing = true;
+    return 0;
+}
+
+int32_t AudioCaptureModule::StopPlayout()
+{
+    _playing = false;
+    return 0;
+}
+
+bool AudioCaptureModule::Playing() const
+{
+    return _playing;
+}
+
+int32_t AudioCaptureModule::StartRecording()
+{
+    if (!_rec_is_initialized) 
+    {
+        return -1;
+    }
+
+    _recording = true;
+    return 0;
+}
+
+int32_t AudioCaptureModule::StopRecording()
+{
+    _recording = false;
+    return 0;
+}
+
+bool AudioCaptureModule::Recording() const
+{
+    return _recording;
+}
+
+int32_t AudioCaptureModule::InitSpeaker()
+{
+    return 0;
+}
+
+bool AudioCaptureModule::SpeakerIsInitialized() const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::InitMicrophone()
+{
+    // No microphone, just playing from file. Return success.
+    return 0;
+}
+
+bool AudioCaptureModule::MicrophoneIsInitialized() const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SpeakerVolumeIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetSpeakerVolume(uint32_t volume)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SpeakerVolume(uint32_t* volume) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MaxSpeakerVolume(uint32_t* maxVolume) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MinSpeakerVolume(uint32_t* minVolume) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MicrophoneVolumeIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetMicrophoneVolume(uint32_t volume)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MicrophoneVolume(uint32_t* volume) const
+{
+    *volume = _current_volume;
+    return 0;
+}
+
+int32_t AudioCaptureModule::MaxMicrophoneVolume(uint32_t* max_volume) const
+{
+    *max_volume = 14392;
+    return 0;
+}
+
+int32_t AudioCaptureModule::MinMicrophoneVolume(uint32_t* min_volume) const
+{
+    *min_volume = 0;
+    return 0;
+}
+
+int32_t AudioCaptureModule::SpeakerMuteIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetSpeakerMute(bool enable)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SpeakerMute(bool* enabled) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MicrophoneMuteIsAvailable(bool* available)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetMicrophoneMute(bool enable)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::MicrophoneMute(bool* enabled) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::StereoPlayoutIsAvailable(bool* available) const
+{
+    *available = true;
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetStereoPlayout(bool enable)
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::StereoPlayout(bool* enabled) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::StereoRecordingIsAvailable(bool* available) const
+{
+    *available = false;
+    return 0;
+}
+
+int32_t AudioCaptureModule::SetStereoRecording(bool enable)
+{
+    if (!enable) 
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int32_t AudioCaptureModule::StereoRecording(bool* enabled) const
+{
+    return 0;
+}
+
+int32_t AudioCaptureModule::PlayoutDelay(uint16_t* delay_ms) const
+{
+    *delay_ms = 0;
+    return 0;
+}
+
+bool AudioCaptureModule::BuiltInAECIsAvailable() const
+{
+    return false;
+}
+
+bool AudioCaptureModule::BuiltInAGCIsAvailable() const
+{
+    return false;
+}
+
+bool AudioCaptureModule::BuiltInNSIsAvailable() const
+{
+    return false;
+}
+
+int32_t AudioCaptureModule::EnableBuiltInAEC(bool enable)
+{
+    return -1;
+}
+
+int32_t AudioCaptureModule::EnableBuiltInAGC(bool enable)
+{
+    return -1;
+}
+
+int32_t AudioCaptureModule::EnableBuiltInNS(bool enable)
+{
+    return -1;
+}
+
+bool AudioCaptureModule::Initialize()
+{
+    _last_process_time_ms = rtc::TimeMillis();
+    return true;
+}
--- batrachiatc/src/audio_capture_module.h
+++ batrachiatc/src/audio_capture_module.h
@@ -10,363 +10,71 @@ class AudioCaptureModule
     , public rtc::RefCountInterface
 {
 public:
-    static AudioCaptureModule* Create()
-    {
-        auto self = new rtc::RefCountedObject<AudioCaptureModule>();
-        if (!self->Initialize()) 
-        {
-            return NULL;
-        }
-
-        self->AddRef();
-        return self;
-    }
-
-    int32_t ActiveAudioLayer(AudioLayer* audioLayer) const
-    {
-        return 0;
-    }
-
-    int32_t RegisterAudioCallback(webrtc::AudioTransport* audio_callback)
-    {
-        _audio_callback = audio_callback;
-        return 0;
-    }
-
-    int32_t Init()
-    {
-        return 0;
-    }
-
-    int32_t Terminate()
-    {
-        // Clean up in the destructor. No action here, just success.
-        return 0;
-    }
-
-    bool Initialized() const
-    {
-        return 0;
-    }
-
-    int16_t PlayoutDevices()
-    {
-        return 0;
-    }
-
-    int16_t RecordingDevices()
-    {
-        return 0;
-    }
-
+    static AudioCaptureModule* Create();
+    int32_t ActiveAudioLayer(AudioLayer* audioLayer) const;
+    int32_t RegisterAudioCallback(webrtc::AudioTransport* audio_callback);
+    int32_t Init();
+    int32_t Terminate();
+    bool Initialized() const;
+    int16_t PlayoutDevices();
+    int16_t RecordingDevices();
     int32_t PlayoutDeviceName(uint16_t index,
         char name[webrtc::kAdmMaxDeviceNameSize],
-        char guid[webrtc::kAdmMaxGuidSize])
-    {
-        return 0;
-    }
-
+        char guid[webrtc::kAdmMaxGuidSize]);
     int32_t RecordingDeviceName(uint16_t index, 
         char name[webrtc::kAdmMaxDeviceNameSize], 
-        char guid[webrtc::kAdmMaxGuidSize])
-    {
-        return 0;
-    }
-
-    int32_t SetPlayoutDevice(uint16_t index)
-    {
-        return 0;
-    }
-
-    int32_t SetPlayoutDevice(WindowsDeviceType device)
-    {
-        if (_play_is_initialized)
-        {
-            return -1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    int32_t SetRecordingDevice(uint16_t index)
-    {
-        return 0;
-    }
-
-    int32_t SetRecordingDevice(WindowsDeviceType device)
-    {
-        if (_rec_is_initialized)
-        {
-            return -1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    int32_t PlayoutIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t InitPlayout()
-    {
-        _play_is_initialized = true;
-        return 0;
-    }
-
-    bool PlayoutIsInitialized() const
-    {
-        return _play_is_initialized;
-}
-
-    int32_t RecordingIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t InitRecording()
-    {
-        _rec_is_initialized = true;
-        return 0;
-    }
-
-    bool RecordingIsInitialized() const
-    {
-        return _rec_is_initialized;
-    }
-
-    int32_t StartPlayout()
-    {
-        if (!_play_is_initialized) 
-        {
-            return -1;
-        }
-
-        _playing = true;
-        return 0;
-    }
-
-    int32_t StopPlayout()
-    {
-        _playing = false;
-        return 0;
-    }
-
-    bool Playing() const
-    {
-        return _playing;
-    }
-
-    int32_t StartRecording()
-    {
-        if (!_rec_is_initialized) 
-        {
-            return -1;
-        }
-
-        _recording = true;
-        return 0;
-    }
-
-    int32_t StopRecording()
-    {
-        _recording = false;
-        return 0;
-    }
-
-    bool Recording() const
-    {
-        return _recording;
-    }
-
-    int32_t InitSpeaker()
-    {
-        return 0;
-    }
-
-    bool SpeakerIsInitialized() const
-    {
-        return 0;
-    }
-
-    int32_t InitMicrophone()
-    {
-        // No microphone, just playing from file. Return success.
-        return 0;
-    }
-
-    bool MicrophoneIsInitialized() const
-    {
-        return 0;
-    }
-
-    int32_t SpeakerVolumeIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t SetSpeakerVolume(uint32_t volume)
-    {
-        return 0;
-    }
-
-    int32_t SpeakerVolume(uint32_t* volume) const
-    {
-        return 0;
-    }
-
-    int32_t MaxSpeakerVolume(uint32_t* maxVolume) const
-    {
-        return 0;
-    }
-
-    int32_t MinSpeakerVolume(uint32_t* minVolume) const
-    {
-        return 0;
-    }
-
-    int32_t MicrophoneVolumeIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t SetMicrophoneVolume(uint32_t volume)
-    {
-        return 0;
-    }
-
-    int32_t MicrophoneVolume(uint32_t* volume) const
-    {
-        *volume = _current_volume;
-        return 0;
-    }
-
-    int32_t MaxMicrophoneVolume(uint32_t* max_volume) const
-    {
-        *max_volume = 14392;
-        return 0;
-    }
-
-    int32_t MinMicrophoneVolume(uint32_t* min_volume) const
-    {
-        *min_volume = 0;
-        return 0;
-    }
-
-    int32_t SpeakerMuteIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t SetSpeakerMute(bool enable)
-    {
-        return 0;
-    }
-
-    int32_t SpeakerMute(bool* enabled) const
-    {
-        return 0;
-    }
-
-    int32_t MicrophoneMuteIsAvailable(bool* available)
-    {
-        return 0;
-    }
-
-    int32_t SetMicrophoneMute(bool enable)
-    {
-        return 0;
-    }
-
-    int32_t MicrophoneMute(bool* enabled) const
-    {
-        return 0;
-    }
-
-    int32_t StereoPlayoutIsAvailable(bool* available) const
-    {
-        *available = true;
-        return 0;
-    }
-
-    int32_t SetStereoPlayout(bool enable)
-    {
-        return 0;
-    }
-
-    int32_t StereoPlayout(bool* enabled) const
-    {
-        return 0;
-    }
-
-    int32_t StereoRecordingIsAvailable(bool* available) const
-    {
-        *available = false;
-        return 0;
-    }
-
-    int32_t SetStereoRecording(bool enable)
-    {
-        if (!enable) 
-        {
-            return 0;
-        }
-        else
-        {
-            return -1;
-        }
-    }
-
-    int32_t StereoRecording(bool* enabled) const
-    {
-        return 0;
-    }
-
-    int32_t PlayoutDelay(uint16_t* delay_ms) const
-    {
-        *delay_ms = 0;
-        return 0;
-    }
-
-    bool BuiltInAECIsAvailable() const
-    {
-        return false;
-    }
-
-    bool BuiltInAGCIsAvailable() const
-    {
-        return false;
-    }
-
-    bool BuiltInNSIsAvailable() const
-    {
-        return false;
-    }
-
-    int32_t EnableBuiltInAEC(bool enable)
-    {
-        return -1;
-    }
-
-    int32_t EnableBuiltInAGC(bool enable)
-    {
-        return -1;
-    }
-
-    int32_t EnableBuiltInNS(bool enable)
-    {
-        return -1;
-    }
+        char guid[webrtc::kAdmMaxGuidSize]);
+    int32_t SetPlayoutDevice(uint16_t index);
+    int32_t SetPlayoutDevice(WindowsDeviceType device);
+    int32_t SetRecordingDevice(uint16_t index);
+    int32_t SetRecordingDevice(WindowsDeviceType device);
+    int32_t PlayoutIsAvailable(bool* available);
+    int32_t InitPlayout();
+    bool PlayoutIsInitialized() const;
+    int32_t RecordingIsAvailable(bool* available);
+    int32_t InitRecording();
+    bool RecordingIsInitialized() const;
+    int32_t StartPlayout();
+    int32_t StopPlayout();
+    bool Playing() const;
+    int32_t StartRecording();
+    int32_t StopRecording();
+    bool Recording() const;
+    int32_t InitSpeaker();
+    bool SpeakerIsInitialized() const;
+    int32_t InitMicrophone();
+    bool MicrophoneIsInitialized() const;
+    int32_t SpeakerVolumeIsAvailable(bool* available);
+    int32_t SetSpeakerVolume(uint32_t volume);
+    int32_t SpeakerVolume(uint32_t* volume) const;
+    int32_t MaxSpeakerVolume(uint32_t* maxVolume) const;
+    int32_t MinSpeakerVolume(uint32_t* minVolume) const;
+    int32_t MicrophoneVolumeIsAvailable(bool* available);
+    int32_t SetMicrophoneVolume(uint32_t volume);
+    int32_t MicrophoneVolume(uint32_t* volume) const;
+    int32_t MaxMicrophoneVolume(uint32_t* max_volume) const;
+    int32_t MinMicrophoneVolume(uint32_t* min_volume) const;
+    int32_t SpeakerMuteIsAvailable(bool* available);
+    int32_t SetSpeakerMute(bool enable);
+    int32_t SpeakerMute(bool* enabled) const;
+    int32_t MicrophoneMuteIsAvailable(bool* available);
+    int32_t SetMicrophoneMute(bool enable);
+    int32_t MicrophoneMute(bool* enabled) const;
+    int32_t StereoPlayoutIsAvailable(bool* available) const;
+    int32_t SetStereoPlayout(bool enable);
+    int32_t StereoPlayout(bool* enabled) const;
+    int32_t StereoRecordingIsAvailable(bool* available) const;
+    int32_t SetStereoRecording(bool enable);
+    int32_t StereoRecording(bool* enabled) const;
+    int32_t PlayoutDelay(uint16_t* delay_ms) const;
+    bool BuiltInAECIsAvailable() const;
+    bool BuiltInAGCIsAvailable() const;
+    bool BuiltInNSIsAvailable() const;
+    int32_t EnableBuiltInAEC(bool enable);
+    int32_t EnableBuiltInAGC(bool enable);
+    int32_t EnableBuiltInNS(bool enable);
 private:
-    bool Initialize()
-    {
-        _last_process_time_ms = rtc::TimeMillis();
-        return true;
-    }
+    bool Initialize();
 
     webrtc::AudioTransport* _audio_callback;
     uint64_t _last_process_time_ms;
