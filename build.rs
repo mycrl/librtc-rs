9918aa5fd84c40948aa0ad83daec6fa536831100
Mr.Panda <xivistudios@gmail.com>
2022-12-17 17:51:25 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -55,27 +55,6 @@ fn download(name: &str) -> (String, String) {
     split(&path)
 }
 
-#[cfg(target_os = "windows")]
-fn link() {
-    println!("cargo:rustc-link-lib=winmm");
-    println!("cargo:rustc-link-lib=secur32");
-    println!("cargo:rustc-link-lib=msdmo");
-    println!("cargo:rustc-link-lib=dmoguids");
-    println!("cargo:rustc-link-lib=wmcodecdspuuid");
-}
-
-#[cfg(target_os = "macos")]
-fn link() {
-    println!("cargo:rustc-link-lib=c++");
-    println!("cargo:rustc-link-lib=framework=Foundation");
-    println!("cargo:rustc-link-lib=framework=AudioToolbox");
-    println!("cargo:rustc-link-lib=framework=AudioUnit");
-    println!("cargo:rustc-link-lib=framework=CoreServices");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rustc-link-lib=framework=CoreAudio");
-    println!("cargo:rustc-link-lib=framework=CoreGraphics");
-}
-
 fn main() {
     dotenv::dotenv().ok();
 
@@ -98,5 +77,28 @@ fn main() {
     println!("cargo:rustc-link-search=all={}", webrtc_lib_path);
     println!("cargo:rustc-link-lib=static={}", sys_lib_name);
     println!("cargo:rustc-link-search=all={}", sys_lib_path);
-    link();
+    
+    if cfg!(windows) {
+        println!("cargo:rustc-link-lib=winmm");
+        println!("cargo:rustc-link-lib=secur32");
+        println!("cargo:rustc-link-lib=msdmo");
+        println!("cargo:rustc-link-lib=dmoguids");
+        println!("cargo:rustc-link-lib=wmcodecdspuuid");
+    }
+
+    if cfg!(macos) {
+        println!("cargo:rustc-link-lib=c++");
+        println!("cargo:rustc-link-lib=framework=Foundation");
+        println!("cargo:rustc-link-lib=framework=AudioToolbox");
+        println!("cargo:rustc-link-lib=framework=AudioUnit");
+        println!("cargo:rustc-link-lib=framework=CoreServices");
+        println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        println!("cargo:rustc-link-lib=framework=CoreAudio");
+        println!("cargo:rustc-link-lib=framework=CoreGraphics");
+    }
+
+    if cfg!(linux) {
+        println!("cargo:rustc-link-lib=stdc++");
+        println!("cargo:rustc-link-lib=X11");
+    }
 }
