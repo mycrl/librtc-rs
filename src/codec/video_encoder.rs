4f67c262b7986c3a45d15c279ecdd886ad985a1a
Mr.Panda <xivistudios@gmail.com>
2022-12-23 04:31:25 +0800

    Update video_encoder.rs

--- src/codec/video_encoder.rs
+++ src/codec/video_encoder.rs
@@ -1,5 +1,6 @@
 use libc::*;
 use std::collections::HashMap;
+use crate::{base::*, abstracts::VectorLayout};
 
 #[repr(C)]
 struct RawParameter {
@@ -11,12 +12,14 @@ struct RawParameter {
 struct RawCodec {
 	name: *const c_char,
 	pars: *const *const RawParameter,
+    capacity: usize,
 	len: usize,
 }
 
 #[repr(C)]
 struct RawCodecs {
 	codecs: *const *const RawCodec,
+    capacity: usize,
 	len: usize,
 }
 
@@ -26,9 +29,36 @@ pub struct VideoEncoder {
 }
 
 pub struct VideoEncoderFactory {
-    codecs: Vec<VideoEncoder>,
+    ptr: *const RawCodecs,
 }
 
 impl VideoEncoderFactory {
-    
+    pub fn new(codecs: Vec<VideoEncoder>) -> Self {
+        let (codecs, len, capacity) = codecs.iter().map(|item| {
+            let mut pars = Vec::new();
+
+            item.parameters.iter().for_each(|(k, v)| {
+                pars.push(Box::into_raw(Box::new(RawParameter {
+                    key: to_c_str(k).unwrap(), 
+                    value: to_c_str(v).unwrap(),
+                })) as *const RawParameter)
+            });
+
+            let (pars, len, capacity) = pars.into_c_layout();
+            Box::into_raw(Box::new(RawCodec {
+                name: to_c_str(&item.name).unwrap(),
+                capacity,
+                pars,
+                len,
+            })) as *const RawCodec
+        }).collect::<Vec<*const RawCodec>>().into_c_layout();
+        
+        Self {
+            ptr: Box::into_raw(Box::new(RawCodecs {
+                codecs,
+                capacity,
+                len,
+            }))
+        }
+    }
 }
